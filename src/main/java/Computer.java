524f8c11142b29f9a90be9e8ec94ca204cbbb58d
mum <mumum3521@naver.com>
2023-12-13 10:43:00 +0900

    Feat computer, refactor player

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,2 +1,56 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 컴퓨터가 숫자를 생성한다.
+ *
+ * @author : mhj94
+ * @see : generateRandomNumber() 난수를 생성한다.
+ * @see : checkStatus() 플레이어 숫자와 비교한다
+ * @see : contains() 플레이어 숫자가 컴퓨터가 생성한 숫자에 포함되어 있는지 비교한다
+ */
 public class Computer {
+
+    private final int[] randomNumber;
+
+    public Computer() {
+        this.randomNumber = generateRandomNumber();
+    }
+
+    private int[] generateRandomNumber() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            numbers.add(i);
+        }
+
+        Collections.shuffle(numbers);
+        int[] shuffleNumber = new int[3];
+        for (int i = 0; i < 3; i++) {
+            shuffleNumber[i] = numbers.get(i);
+        }
+        return shuffleNumber;
+    }
+
+    public int[] checkStatus(int[] playerNumber) {
+        int[] result = new int[2];
+
+        for (int i = 0; i < 2; i++) {
+            if (randomNumber[i]==result[i]) {
+                result[0]++;
+            } else if (contains(randomNumber,result[i])) {
+                result[1]++;
+            }
+        }
+        return result;
+    }
+
+    private boolean contains(int[] randomNumber, int i) {
+        for (int number : randomNumber) {
+            if (number == i) {
+               return true;
+            }
+        }
+        return false;
+    }
 }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,17 +1,26 @@
 import java.util.Scanner;
 
+/**
+ * 플레이어가 숫자를 입력한다.
+ *
+ * @author : hyun
+ * @return : 입력한 숫자
+ * @see : choice() 입력한 숫자를 받아, 쪼개서 배열에 저장
+ */
 public class Player {
-    // 필드
-    int choiceNum;
-    int[] choiceArr;
-
-    // 메서드
     public int[] choice() {
         Scanner scan = new Scanner(System.in);  // 입력받을 수 있게
+        System.out.println("세자리 숫자 입력해주세요");
+
         int choiceNum = scan.nextInt(); // 입력받은 숫자 -> int 타입 choiceNum
         String choiceNumStr = String.valueOf(choiceNum); // choiceNum -> String타입
 
         int[] choiceNumArr = new int[choiceNumStr.length()]; // 반환할 int배열 생성
+
+        if((choiceNumStr.length() !=3) || !choiceNumStr.matches("\\d{3}")) {
+            System.out.println("세자리 숫자로 입력해주세요");
+            return choice();
+        }
 
         // String타입으로 바꾼 입력값을 글자마다 쪼개서 int배열에 저장
         for (int i=0; i<choiceNumStr.length(); i++) {
@@ -19,14 +28,6 @@ public class Player {
         }
 
         return choiceNumArr;
-
     }
 
-    public static void main(String[] args) {
-        Player player = new Player();
-        int[] choice = player.choice();
-        System.out.print(choice[0]);
-        System.out.print(choice[1]);
-        System.out.print(choice[2]);
-    }
 }
